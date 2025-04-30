30f5ab4a269b57699eb78e048cd349f000de9483
yehdanny <chinochinowind@gmail.com>
2025-04-30 17:21:55 +0800

    2025-4-30:4

---
+++ RandomForest_demo.py
@@ -0,0 +1,17 @@
+import mlflow
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_diabetes
+from sklearn.ensemble import RandomForestRegressor
+remote_server_uri = "http://127.0.0.1:8080"  #mlflow server --host 127.0.0.1 --port 8080
+mlflow.sklearn.autolog()
+
+db = load_diabetes()
+X_train, X_test, y_train, y_test = train_test_split(db.data, db.target)
+
+# Create and train models.
+rf = RandomForestRegressor(n_estimators=100, max_depth=6, max_features=3)
+rf.fit(X_train, y_train)
+
+# Use the model to make predictions on the test dataset.
+predictions = rf.predict(X_test)
